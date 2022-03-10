fbafe3b48ad7ebd708b4ae6eca99f722ebd182e0
hoxirious <giahy33@gmail.com>
2022-03-09 17:24:48 -0700

    feat: user controller

--- Backend/functions/src/modules/users/users.controller.ts
+++ Backend/functions/src/modules/users/users.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { UserRecord } from "firebase-functions/v1/auth";
+import { FirebaseUser } from "src/nestjs/decorators/firebase-user.decorator";
 import { UserInfoSchema } from "src/schemas/users/userInfo.schema";
 import { UsersService } from "./users.service";
 
@@ -10,5 +12,10 @@ export class UsersController {
   async getUser(): Promise<UserInfoSchema> {
     console.log("Getting users...");
     return await UsersService.getUser();
+  }
+
+  @Post()
+  async createUser(@FirebaseUser() user: UserRecord, @Body() body: UserInfoSchema ): Promise<void> {
+    
   }
 }
