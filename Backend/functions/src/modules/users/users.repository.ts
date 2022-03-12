935c56aa687de2103ff665ab5f9b4c0a952d8719
Hy Huynh <giahy33@gmail.com>
2022-03-12 11:26:20 -0700

    feat: create mocking request on Postman test: tested getUser and createUser

--- Backend/functions/src/modules/users/users.repository.ts
+++ Backend/functions/src/modules/users/users.repository.ts
@@ -3,20 +3,22 @@ import { db } from "../../firebase/repository.firebase";
 import { UserInfoSchema } from "../../schemas/users/userInfo.schema";
 
 export class UsersRepository {
-
   static async getUser(id: string): Promise<UserInfoSchema> {
-    const ToReturn = await db.users.where("userID", "==", id).get();
+    const query = await db.users.where("userID", "==", id).get();
 
-    // todos: Handle error
-    const result: UserInfoSchema[] = ToReturn.docs.map(
-      (each: QueryDocumentSnapshot) => each.data() as UserInfoSchema
-    );
-    return result[0];
+    let ToReturn: UserInfoSchema;
+    query.docs.forEach((each: QueryDocumentSnapshot) => {
+      ToReturn = each.data() as UserInfoSchema;
+    });
+
+    if (ToReturn) return ToReturn;
+    else throw new Error("user/not-found");
   }
 
   static async postUser(
     user: UserInfoSchema
   ): Promise<FirebaseFirestore.WriteResult> {
+    console.log(user);
     return await db.users.doc().set(user);
   }
 }
