a190e48525e33d282744e5b88033f35a545e7056
Hy Huynh <giahy33@gmail.com>
2022-03-23 13:35:33 -0600

    feat: getUserStream

--- Backend/functions/src/modules/events/events.repository.ts
+++ Backend/functions/src/modules/events/events.repository.ts
@@ -1,7 +1,15 @@
+import { db } from "../../firebase/repository.firebase";
 import { EventInfoSchema } from "../../schemas/events/eventInfo.schema";
 
 export class EventsRepository {
-  static async getUserStream(userID: string) {}
+  static async getUserStream(userID: string): Promise<EventInfoSchema[]> {
+    try {
+      return (await db.events.doc(userID).get()).data();
+    } catch (error) {
+      throw new Error("error: cannot retrieve user stream");
+    }
+  }
+
   static async postEvent(userID: string, eventPayload: EventInfoSchema) {}
-  static async dispatchAction(userID: string, eventPayload: EventInfoSchema){}
+  static async dispatchAction(userID: string, eventPayload: EventInfoSchema) {}
 }
