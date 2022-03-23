75e084bf587e6410371d6aad3cbe13c85f4d51fe
Hy Huynh <giahy33@gmail.com>
2022-03-23 13:57:13 -0600

    feat: postEvent

--- Backend/functions/src/modules/events/events.repository.ts
+++ Backend/functions/src/modules/events/events.repository.ts
@@ -1,6 +1,7 @@
 import { db } from "../../firebase/repository.firebase";
 import { EventInfoSchema } from "../../schemas/events/event-info.schema";
 import { StreamInfoSchema } from "../../schemas/events/stream-info.schema";
+import { firestore } from "firebase-admin";
 
 export class EventsRepository {
   static async getUserStream(userID: string): Promise<StreamInfoSchema[]> {
@@ -11,6 +12,14 @@ export class EventsRepository {
     }
   }
 
-  static async postEvent(userID: string, eventPayload: EventInfoSchema) {}
+  static async postEvent(userID: string, eventPayload: EventInfoSchema) {
+    try {
+      await db.events.doc(userID).update({
+        events: firestore.FieldValue.arrayUnion(eventPayload)
+      })
+    } catch (error) {
+      throw new Error("error: cannot post new event into user stream!");
+    }
+  }
   static async dispatchAction(userID: string, eventPayload: EventInfoSchema) {}
 }
