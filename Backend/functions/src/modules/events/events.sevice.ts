2511f9e30eceab9eb8e18d52285b277691b12909
Hy Huynh <giahy33@gmail.com>
2022-03-24 15:54:34 -0600

    feat:  billing action + FromFriend + fromGroup

--- Backend/functions/src/modules/events/events.sevice.ts
+++ Backend/functions/src/modules/events/events.sevice.ts
@@ -5,7 +5,24 @@ import { GroupInfoSchema } from "../../schemas/groups/groupInfo.schema";
 import { EventType } from "./definitions/event-type.definition";
 import { EventsRepository } from "./events.repository";
 
+const isExpenseType = (eventType: EventType): boolean =>
+  eventType === EventType.ExpenseCreate ||
+  eventType === EventType.ExpenseDelete ||
+  eventType === EventType.ExpenseUndelete ||
+  eventType === EventType.ExpenseUpdate;
+
 export class EventsService {
+  static billingAction(eventType: EventType, userLentAmount: number): number {
+    switch (eventType) {
+      case EventType.ExpenseCreate:
+        return userLentAmount;
+      case EventType.ExpenseDelete:
+        return -userLentAmount;
+      default:
+        throw new Error("event type does not support");
+    }
+  }
+
   /**
    *
    * @param userID
@@ -18,19 +35,18 @@ export class EventsService {
 
     //* Loop through the event list to total up the current balance of the user
     return userStream.eventList.reduce<number>((currentBalance, event) => {
-      if (
-        event.eventType === EventType.ExpenseCreate ||
-        event.eventType === EventType.ExpenseDelete ||
-        event.eventType === EventType.ExpenseUndelete ||
-        event.eventType === EventType.ExpenseUpdate
-      ) {
+      //* Assert event.eventContent type into ExpenseInfoSchema
+      const eventContent = event.eventContent as ExpenseInfoSchema;
+      if (isExpenseType(event.eventType)) {
         //* Assert event.eventContent type into ExpenseInfoSchema to retrieve events that relates to userID
-        const userBilling = (
-          event.eventContent as ExpenseInfoSchema
-        ).splitDetail.find((user) => user.userID === userID);
-
+        const userBilling = eventContent.splitDetail.find(
+          (user) => user.userID === userID
+        );
         //* Increment the amount into currentBalance
-        currentBalance += userBilling.lentAmount;
+        currentBalance += this.billingAction(
+          event.eventType,
+          userBilling.lentAmount
+        );
       }
       return currentBalance;
     }, 0);
@@ -41,6 +57,7 @@ export class EventsService {
     groupID: string
   ): Promise<number> {
     const userStream = await EventsRepository.getUserStream(userID);
+
     //* Loop through the event list to total up the current balance of the user
     return userStream.eventList.reduce<number>((currentBalance, event) => {
       //* Assert event.eventContent type into ExpenseInfoSchema
@@ -49,10 +66,7 @@ export class EventsService {
       //* If event has groupReference and its type is expense
       if (
         eventContent.groupReference === groupID &&
-        (event.eventType === EventType.ExpenseCreate ||
-          event.eventType === EventType.ExpenseDelete ||
-          event.eventType === EventType.ExpenseUndelete ||
-          event.eventType === EventType.ExpenseUpdate)
+        isExpenseType(event.eventType)
       ) {
         //*  Retrieve events that relates to userID
         const userBilling = eventContent.splitDetail.find(
@@ -60,7 +74,43 @@ export class EventsService {
         );
 
         //* Increment the amount into currentBalance
-        currentBalance += userBilling.lentAmount;
+        currentBalance += this.billingAction(
+          event.eventType,
+          userBilling.lentAmount
+        );
+      }
+      return currentBalance;
+    }, 0);
+  }
+
+  /**
+   *
+   * @param userID
+   * @param friendID
+   * @returns Get the current balance after billing calculation with the friendID
+   */
+  static async getCurrentBalanceFromFriend(userID: string, friendID: string) {
+    const userStream = await EventsRepository.getUserStream(userID);
+    //* Loop through the event list to total up the current balance of the user
+    return userStream.eventList.reduce<number>((currentBalance, event) => {
+      //* Assert event.eventContent type into ExpenseInfoSchema
+      const eventContent = event.eventContent as ExpenseInfoSchema;
+
+      //* If event has groupReference and its type is expense
+      if (isExpenseType(event.eventType)) {
+        //*  Retrieve billing that relates to userID
+        const userBilling = eventContent.splitDetail.find(
+          (user) => user.userID === userID
+        );
+        const friendBilling = eventContent.splitDetail.find(
+          (user) => user.userID === friendID
+        );
+
+        //* Increment the amount into currentBalance
+        currentBalance += this.billingAction(
+          event.eventType,
+          userBilling.lentAmount + friendBilling.lentAmount
+        );
       }
       return currentBalance;
     }, 0);
