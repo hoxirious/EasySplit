5ae1127ac6275c87dd58234ac306dc8ee531bf9d
Hy Huynh <giahy33@gmail.com>
2022-03-24 18:53:53 -0600

    feat: implement split Expense

--- Backend/functions/src/modules/expenses/expenses.controller.ts
+++ Backend/functions/src/modules/expenses/expenses.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
+import { BillingInfoSchema } from "../../schemas/expenses/billingInfo.schema";
 import { ExpenseInfoSchema } from "../../schemas/expenses/expenseInfo.schema";
+import { GetSplitBillingBodyPayment } from "./dtos/get-splitBillingPayment.dto";
 import { PostExpenseBodyDto } from "./dtos/post-expense.dto";
 import { PutExpenseBodyDto } from "./dtos/put-expense.dto";
 import { ExpensesService } from "./expenses.service";
 
 @Controller("expense")
 export class ExpensesController {
-
   @Put("/update/:expenseID")
   async updateExpenseByID(
     @Param("expenseID") expenseID: string,
@@ -37,5 +38,10 @@ export class ExpensesController {
   ): Promise<ExpenseInfoSchema[]> {
     console.log("Getting expense by groupID...");
     return await ExpensesService.getExpenseByGroupID(groupID);
+  }
+
+  @Get("/splitExpense")
+  splitExpense(@Body() body: GetSplitBillingBodyPayment): BillingInfoSchema[] {
+    return ExpensesService.splitExpense(body);
   }
 }
