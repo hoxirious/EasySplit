edf06a70cac5bd4873ef9eb4346657dd1f7d6a53
PriyavartRajain <47432873+PriyavartRajain@users.noreply.github.com>
2022-03-25 17:23:55 -0600

    Added comments

--- Frontend/easysplit/src/Components/AddExpenseModal.js
+++ Frontend/easysplit/src/Components/AddExpenseModal.js
@@ -7,62 +7,63 @@ import percent from "../Resources/percent.png"
 
 function AddExpenseModal(props) {
 
-    const [splitMethod, setSplitMethod] = useState("equal");    // 1
-    const [splitWithArr, setSplitWithArr] = useState([]);       // 2
-    const [amountAfterSplit, setAmountAfterSplit] = useState(0.0);  //3
-    const [exactAmountsAfterSplit, setExactAmountsAfterSplit] = useState([]);   //4
-    const [amountsAfterPercentSplit, setAmountsAfterPercentSplit] = useState([]);   //5
-    const [finalSplitAmount, setFinalSplitAmount] = useState([]); //6
-    const [returnObj, setReturnObj] = useState({}); //7
+    const [splitMethod, setSplitMethod] = useState("equal");                        // Stores the split method   
+    const [splitWithArr, setSplitWithArr] = useState([]);                           // Stores the emails of the people the bill is being split with
+    const [amountAfterSplit, setAmountAfterSplit] = useState(0.0);                  // Stores the split amount when the split method is "equal"
+    const [exactAmountsAfterSplit, setExactAmountsAfterSplit] = useState([]);       // Stores the split amount for each person when the split method is "exact"
+    const [amountsAfterPercentSplit, setAmountsAfterPercentSplit] = useState([]);   // Stores the split amount for each person when the split method is "percent"
+    const [finalSplitAmount, setFinalSplitAmount] = useState([]);                   // Stores the split amount for each person after they are done choosing a split method. i.e stores the latest split amounts 
 
-    const modal = useRef();
-    const splitInfoDiv = useRef();
-    const shareWithInput = useRef();
-    const amount = useRef();
-    const splitBtn = useRef();
-    const saveBtn = useRef();
-    const paidAmountsDiv = useRef();
+    const modal = useRef();                                                         // Refers to the addExpense modal that covers the entire screen
+    const splitInfoDiv = useRef();                                                  // Refers to the div that contains the list of emails and the respective split amount
+    const shareWithInput = useRef();                                                // Refers to the input that takes in the emails
+    const amount = useRef();                                                        // Refers to the total bill amount
+    const splitBtn = useRef();                                                      // Refers to the split button
+    const saveBtn = useRef();                                                       // Refers to the save button
+    const paidAmountsDiv = useRef();                                                // Refers to the div that contains the list of emails and the respective amount that they actually paid
 
-    function addEmails() {
+    function addEmails() {                                                          // Splits the email input into different emails with ',' being the delimitter
         let emails = (shareWithInput.current.value).split(",");
-        setSplitWithArr(emails);
-        //console.log(splitWithArr);
+        setSplitWithArr(emails);                                                    // Update the state
     }
 
+    /*Executes when split button is clicked.
+      Calculates the split amount based on the split method i.e exact, or percent (does not handle the "equal" method case)
+      Once the split amount is computed, it updates the DOM elements to display the same
+    */
     function computeSplit() {
-        let ul = splitInfoDiv.current.children[0];
-        let lis = ul.getElementsByTagName("li");
-        //console.log(lis);
+        let ul = splitInfoDiv.current.children[0];          // The first child of splitinfoDiv is an unordered list
+        let lis = ul.getElementsByTagName("li");            // Grabs all teh list items within that list. These list items are supposed to contain the email and the input that takes in the respective split amount (exact value or percent)
+
         if (splitMethod === 'exact') {
             let exactValues = [];
             for (let li of lis) {
-                exactValues.push(li.children[1].value)
+                exactValues.push(li.children[1].value)     // Since split method is exact, we grab the input value and push it inside the exact values array
             }
-            setExactAmountsAfterSplit(exactValues);
-            setFinalSplitAmount(exactValues);
+            setExactAmountsAfterSplit(exactValues);        // Update the array that contains split amounts when split method is exact 
+            setFinalSplitAmount(exactValues);              // Update the latest split amounts array
         }
-        else if (splitMethod === 'percent') {
-            let totalAmount = amount.current.value;
-            //console.log(totalAmount);
+        else if (splitMethod === 'percent') {              // Do the same as above but when split method is percent
+            let totalAmount = amount.current.value;        // Get the total bill amount because we need it to compute the split amount for percent method 
             let valuesAfterSplit = [];
             for (let li of lis) {
-                valuesAfterSplit.push(((li.children[1].value) / 100) * totalAmount)
+                valuesAfterSplit.push(((li.children[1].value) / 100) * totalAmount)     // Compute the split amount based on the percent value in the input field
                 const node = document.createElement("span");
-                const textnode = document.createTextNode(`$` + `${(((li.children[1].value) / 100) * totalAmount).toFixed(2)}`);
+                const textnode = document.createTextNode(`$` + `${(((li.children[1].value) / 100) * totalAmount).toFixed(2)}`); // Make a DOM element that will display split amounts 
                 node.appendChild(textnode);
-                if (!li.children[2]) {
+                if (!li.children[2]) {                                                  // If the split amount has not been computed yet, it will append the split amount to the list
                     li.appendChild(node);
                 }
-                else {
+                else {                                                                   // If it has been computed before, it will change the split amount to the latest computed one
                     li.children[2].innerText = `$` + `${(((li.children[1].value) / 100) * totalAmount).toFixed(2)}`;
                 }
             }
-            setAmountsAfterPercentSplit(valuesAfterSplit);
-            setFinalSplitAmount(valuesAfterSplit);
+            setAmountsAfterPercentSplit(valuesAfterSplit);                          // Update the array that contains teh split amounts when the split method is "percent"
+            setFinalSplitAmount(valuesAfterSplit);                                  // Update the latest split amounts array
         }
 
-        populatePaidAmountsDiv();
-        saveBtn.current.style.display = 'block';
+        populatePaidAmountsDiv();                                                   // Display the div that contains the list of emails and an input field next to each to get the amount paid input
+        saveBtn.current.style.display = 'block';                                    // Display the save button
     }
 
     function populatePaidAmountsDiv() {
@@ -76,30 +77,36 @@ function AddExpenseModal(props) {
             `
     }
 
+    /*Grabs the value of each input field that contains the amount paid by each person */
     function updatePaidAmounts() {
-        let ul = paidAmountsDiv.current.children[2];
+        let ul = paidAmountsDiv.current.children[2];        // The second element in the paid amounts div is an unordered list. (The first two are an <hr> and a <h5> as shown in the previous function)
         let lis = ul.getElementsByTagName("li");
         let paidAmounts = [];
         for (let li of lis) {
             paidAmounts.push(li.children[1].value * 1)
         }
-        prepareReturnObj(paidAmounts);
+        prepareReturnObj(paidAmounts);                      // Send the paid amounts to the function that prepares the object to be returned
     }
 
+    /*Executes when one of the split method icons is clicked and the split method name is provided as argument 
+      Based on the chosen split method, displays the emails and an input field next to each email (Except when the split method is 'equal' since that does not require a user input)
+    */
     function changeSplitMethod(splitMethod) {
         setSplitMethod(splitMethod);
         if (splitMethod === 'equal') {
-            splitBtn.current.style.display = 'none';
+            splitBtn.current.style.display = 'none';            // Hides the split button in case the split method is equal because we don't need it in this case
             setAmountAfterSplit(() => {
-                if (splitWithArr.length < 2) {
+                if (splitWithArr.length < 2) {                  // If there is 0 or 1 emails, the split amount remains the same as the total bill amount
                     setFinalSplitAmount(amount.current.value);
                     return amount.current.value;
                 }
                 else {
-                    setFinalSplitAmount((amount.current.value / splitWithArr.length).toFixed(2));
+                    setFinalSplitAmount((amount.current.value / splitWithArr.length).toFixed(2));   // If emails >=2 split the total amount equally between all emails
                     return (amount.current.value / splitWithArr.length).toFixed(2);
                 }
             });
+
+            /*Prepare the unordered list that will contain the emails and the respective split amount next to each email*/
             let emailList = splitWithArr.map(email => `<li style="text-align:left"> <span style='color: #2bbbad; font-weight: 500'>${email}</span> <span style='font-weight: 500; color: "black"'>$${(amount.current.value / splitWithArr.length).toFixed(2)}</span></li>`).join('');
             splitInfoDiv.current.innerHTML = `
             <ul style="list-style-type:none;margin:0;padding:0">
@@ -109,6 +116,8 @@ function AddExpenseModal(props) {
         }
         else if (splitMethod === 'exact') {
             splitBtn.current.style.display = 'block';
+
+            /*Prepare the unordered list that will contain the emails and an input field next to each email to take the exact amount input */
             let emailList = splitWithArr.map(email => `<li style="text-align:left"> <span style='color: #2bbbad; font-weight: 500'>${email}</span><input className="formControl" placeholder="$" style='margin: 1em 1em 0 1em; padding-left: 0.5em; width: 4em'/></li>`).join('');
             splitInfoDiv.current.innerHTML = `
             <ul style="list-style-type:none;margin:0;padding:0">
@@ -118,6 +127,7 @@ function AddExpenseModal(props) {
         }
         else if (splitMethod === 'percent') {
             splitBtn.current.style.display = 'block';
+            /*Prepare the unordered list that will contain the emails and an input field next to each email to take the percentage input*/
             let emailList = splitWithArr.map(email => `<li style="text-align:left"> <span style='color: #2bbbad; font-weight: 500'>${email}</span><input className="formControl" placeholder="%" style='margin: 1em 1em 0 1em; padding-left: 0.5em; width: 4em'/></li>`).join('');
             splitInfoDiv.current.innerHTML = `
             <ul style="list-style-type:none;margin:0;padding:0">
@@ -126,6 +136,9 @@ function AddExpenseModal(props) {
             `
         }
     }
+
+    /*Prepares and returns an array of objects. 
+    Each object contains an email, the respective split amount, and the paid amount*/
 
     function prepareReturnObj(paidAmounts) {
         let returnObj = [];
@@ -139,6 +152,7 @@ function AddExpenseModal(props) {
             returnObj.push(obj)
         }
         console.log(returnObj);
+        modal.current.style.display = 'none'
         return returnObj
     }
 
