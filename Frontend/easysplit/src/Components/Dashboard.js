7e2ffd312e9287881cb5fa9a86f6d0cf82591a22
PriyavartRajain <47432873+PriyavartRajain@users.noreply.github.com>
2022-03-26 04:47:15 -0600

    implemented all-expenses

--- Frontend/easysplit/src/Components/Dashboard.js
+++ Frontend/easysplit/src/Components/Dashboard.js
@@ -10,6 +10,7 @@ function Dashboard(props) {
     const [addFriend, setAddFriend] = useState(false);
     const [addGroup, setAddGroup] = useState(false);
     const [addExpense, setAddExpense] = useState(false);
+    const [allExpenses, setAllExpenses] = useState(false);
     return (
         <div className="homeScreenDiv">
             <nav id="mainNavbar" className="navbar navbar-dark bg-dark py-0 px-0 fixed-top d-flex">
@@ -18,8 +19,8 @@ function Dashboard(props) {
                 </div>
             </nav>
             <div className="dashBoardDiv">
-                <LeftSideBar toggleAddFriendModal={setAddFriend} toggleAddGroupModal={setAddGroup} />
-                <MiddleBar toggleAddExpense={setAddExpense} />
+                <LeftSideBar toggleAddFriendModal={setAddFriend} toggleAddGroupModal={setAddGroup} toggleAllExpenses={setAllExpenses} />
+                <MiddleBar isAllExpensesOpen={allExpenses} toggleAddExpense={setAddExpense} />
                 <RightSidebar />
                 <AddFriendModal isOpen={addFriend} toggleAddFriendModal={setAddFriend} />
                 <AddGroupModal isOpen={addGroup} toggleAddGroupModal={setAddGroup} />
--- Frontend/easysplit/src/Components/MiddleBar.js
+++ Frontend/easysplit/src/Components/MiddleBar.js
@@ -1,5 +1,20 @@
-import React from "react"
+import { React, useEffect, useRef } from "react"
+import easysplitlogo from '../Resources/divided.png'
+import AllExpenses from "./AllExpenses"
+
 function MiddleBar(props) {
+
+    const heading = useRef();
+
+    useEffect(() => {
+        if (props.isAllExpensesOpen) {
+            heading.current.style.display = 'none';
+        }
+        else {
+            heading.current.style.display = 'block';
+        }
+    }, [props.isAllExpensesOpen]);
+
     return (
 
         <div className="centerDiv">
@@ -8,12 +23,13 @@ function MiddleBar(props) {
                 <div id="#topbar-actions" className="topbar-actions-group">
                     <button id="add-expense-btn" onClick={() => props.toggleAddExpense(true)}>Add an expense</button>
                     <button id="settleup-btn">Settle up</button>
-                </div>  
+                </div>
             </div>
             <div className="all-expenses">
-                <h1 style={{ color: "black", margin: 30 }}>You have not added any expenses yet</h1>
-                {/* Expenses list will be added to the innerHtml of this div*/}
+                <h1 style={{ color: "black", margin: 30 }} ref={heading}>You have not added any expenses yet</h1>
+                <AllExpenses isOpen={props.isAllExpensesOpen} />
             </div>
+
         </div>
     )
 }
